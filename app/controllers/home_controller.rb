43d08f580a7e77788d4b781c1fb73d508cee079e
rekhaanaya <rekhu.meena@gmail.com>
2021-04-19 13:35:13 -0700

    Rails project code fpr Friends App

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+
+  	@about_me = "This is about me"
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  #devise_for :installs
+  resources :friends
+  get 'home/about'
+  root 'friends#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
